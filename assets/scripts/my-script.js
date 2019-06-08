af40501b063d9f53da50d7bba33c11dad8d4acb0
Andy <andrea_sue2@icloud.com>
2019-06-08 14:37:10 -0500

    Replaced string concat with template literals

--- assets/scripts/my-script.js
+++ assets/scripts/my-script.js
@@ -1,13 +1,62 @@
+const cocktailsArray = [{
+         'id': 'focusedLady',
+         'label': 'Focused Lady'
+     },
+     {
+         'id': 'strongLady',
+         'label': 'Strong Lady'
+     },
+     {
+         'id': 'frontEndPunch',
+         'label': 'Front-End Punch'
+     },
+     {
+         'id': 'cachedOut',
+         'label': 'Cached Out'
+     },
+     {
+         'id': 'httPapaya',
+         'label': 'httPAPAYA://'
+     },
+     {
+         'id': 'nerdyDaiquiri',
+         'label': 'Nerdy Daiquiri'
+     },
+     {
+         'id': 'theAvernaCode',
+         'label': 'The Averna Code'
+     },
+     {
+         'id': 'focusedTheMostest',
+         'label': 'Focused the Mostest'
+     },
+ ];
+
+// why am I not calling this a function??
+ const buildCocktailsMenu = (cocktails) => {
+   let cocktailsString = '';
+
+   cocktails.forEach((cocktail) => {
+     cocktailsString += `<label class="radio" for="${cocktail.id}"><input type="radio" id="${cocktail.id}" name="drink" value="${cocktail.label}">${cocktail.label}</label>`;
+    })
+
+   document.querySelector('.radio-group').innerHTML = cocktailsString;
+ };
+
 const submitOrder = (name, drink) => {
-  alert(name + " is thirsty for a " + drink);
+  let newNode = document.createElement('h3');
+  const newText = document.createTextNode(`${name} would like a ${drink}`);
+  newNode.appendChild(newText);
+  document.querySelector('.order-details').appendChild(newNode);
 };
 
-// document.getElementById('order-btn').addEventListener('click', () => {
-//       submitOrder(document.getElementById('order-form-input').value);
-// });
+document.addEventListener("DOMContentLoaded", (event) => {
 
-document.getElementById('order-btn').addEventListener('click', () => {
-  var name = document.getElementById('order-form-input').value;
-  var drink = document.querySelector('input[type="radio"]:checked').value;
-  submitOrder(name, drink);
+  buildCocktailsMenu(cocktailsArray);
+
+  document.getElementById('order-btn').addEventListener('click', () => {
+    var name = document.getElementById('order-form-input').value;
+    var drink = document.querySelector('input[type="radio"]:checked').value;
+    submitOrder(name, drink);
+  });
 });
